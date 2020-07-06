a0dfac80e95a4c982d1a6763a886378a25c516e4
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2020-07-06 11:51:20 +0100

    Tidy up code

--- fetchDataLambda/index.js
+++ fetchDataLambda/index.js
@@ -3,8 +3,8 @@ const fetch = require('node-fetch');
 exports.handler = async function(event, context, callback) {
 
     // fetch some data from a REST API
-    const response = await fetch("https://putsreq.com/5BG4sgQxkftAzQNkrozi");
-    const json = await response.json();
+    const rawHttpResponse = await fetch("https://putsreq.com/5BG4sgQxkftAzQNkrozi");
+    const responseBody = await rawHttpResponse.json();
 
     // return the data we just received
     callback(null, {
