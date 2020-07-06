47c69150dfb47baf1da908b6488d30e9981adc06
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2020-07-06 15:43:12 +0100

    Tidy up code

--- infrastructure/lib/cdk-stack.ts
+++ infrastructure/lib/cdk-stack.ts
@@ -10,6 +10,7 @@ export class CdkStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
+    // set up a lambda using the source code in 'fetchDataLambda' project
     const fetchDataLambda = new lambda.Function(this, 'apiDemoFetchDataFunction', {
       runtime: lambda.Runtime.NODEJS_12_X,
       handler: 'index.handler',
@@ -19,7 +20,7 @@ export class CdkStack extends cdk.Stack {
     // allow lambda to be invoked by API Gateway
     fetchDataLambda.grantInvoke(new ServicePrincipal('apigateway.amazonaws.com'));
 
-    // fetch OpenAPI specification
+    // load the OpenAPI specification
     const openApiSpecJson = readFileSync("../openApiSpecification.json", 'utf8');
     const openApiSpec = JSON.parse(openApiSpecJson);
 
@@ -27,7 +28,7 @@ export class CdkStack extends cdk.Stack {
     openApiSpec["paths"]["/courses"]["get"]["x-amazon-apigateway-integration"] = {
       uri: `arn:aws:apigateway:${process.env.CDK_DEFAULT_REGION}:lambda:path/2015-03-31/functions/${fetchDataLambda.functionArn}/invocations`,
       type: "aws_proxy",
-      httpMethod: "POST",
+      httpMethod: "POST",  // note that lambda proxy integrations require "POST" http method (see https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-swagger-extensions-integration.html)
       passthroughBehavior: "when_no_match"
     };
 
