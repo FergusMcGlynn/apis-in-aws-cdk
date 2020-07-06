219d8966137dd4e71f1f6cb6c79ffd259b6656ad
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2020-07-06 15:42:38 +0100

    Tidy up code

--- infrastructure/lib/cdk-stack.ts
+++ infrastructure/lib/cdk-stack.ts
@@ -8,15 +8,21 @@ export class CdkStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
+    // set up a lambda using the source code in 'fetchDataLambda' project
     const fetchDataLambda = new lambda.Function(this, 'apiDemoFetchDataFunction', {
       runtime: lambda.Runtime.NODEJS_12_X,
       handler: 'index.handler',
       code: lambda.Code.fromAsset("../fetchDataLambda"),
     });
 
+    // set up a REST API
     const api = new apiGateway.RestApi(this, 'apiDemoRestApi');
+
+    // define a /courses endpoint for the REST API
     const coursesEndpoint = api.root.addResource('courses');
-    coursesEndpoint.addMethod('GET', new apiGateway.LambdaIntegration(fetchDataLambda));  // GET /courses
+
+    // handle GET requests to the /courses endpoint with the lambda
+    coursesEndpoint.addMethod('GET', new apiGateway.LambdaIntegration(fetchDataLambda));
 
   }
 }
