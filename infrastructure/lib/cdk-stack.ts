d763e1709fc2d1af938258e0b5b78366e5869609
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2020-07-06 11:58:59 +0100

    Add Basic API to trigger the lambda

--- infrastructure/lib/cdk-stack.ts
+++ infrastructure/lib/cdk-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as apiGateway from '@aws-cdk/aws-apigateway';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -11,6 +12,10 @@ export class CdkStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_12_X,
       handler: 'index.handler',
       code: lambda.Code.fromAsset("../fetchDataLambda"),
+    });
+
+    new apiGateway.LambdaRestApi(this, 'apiDemoRestApi', {
+      handler: fetchDataLambda,
     });
 
   }
